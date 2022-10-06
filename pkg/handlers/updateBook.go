b7f810ae7f96cf27d4b8bbb3ddbdece7d7cf2f27
mrmilul <miladmizani@yahoo.com>
2022-10-06 20:11:17 +0300

    Impeove UpdateBook

--- pkg/handlers/updateBook.go
+++ pkg/handlers/updateBook.go
@@ -27,6 +27,11 @@ func (h handler) UpdateBook(w http.ResponseWriter, r *http.Request) {
 	if result := h.DB.Find(&book, id); result.Error != nil {
 		fmt.Println(result.Error)
 	}
+	book.Title = updatedBook.Title
+	book.Author = updatedBook.Author
+	book.Desc = updatedBook.Desc
+	h.DB.Save(&book)
+
 	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode("updated")
